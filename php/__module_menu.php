e00ad04e273ba1891618c16b54f4365836a75ef8
Tsjippy <ewaldha@hotmail.com>
2024-10-04 14:32:36 +0100

    changed: readme

--- php/__module_menu.php
+++ php/__module_menu.php
@@ -14,30 +14,6 @@ add_action('sim_module_activated', function($moduleSlug){
 	wp_create_category('Confidential');
 });
 
-add_filter('sim_submenu_description', function($description, $moduleSlug){
-	//module slug should be the same as the constant
-	if($moduleSlug != MODULE_SLUG)	{
-		return $description;
-	}
-
-	ob_start();
-	?>
-	<p>
-		This module filters all content to be only available to logged-in users.<br>
-		Only content with the public category is visible to non-logged-in users.<br>
-		It also makes it possible to move files to a private folder so that it is not directly accessable.<br>
-		<br>
-		It adds one shortcode: 'content_filter' which makes it possible to limit certain parts of a page or post to certain groups.<br>
-		This shortcode has two properties: roles and inversed.<br>
-		Roles define the roles who can see the content.<br>
-		If inversed is set to true, roles define the roles who cannot see the content.<br>
-		Use like this: <code>[content_filter roles='administrator, otherroles']This has limited visibility[/content_filter]</code>
-	</p>
-	<?php
-
-	return ob_get_clean();
-}, 10, 2);
-
 add_filter('sim_submenu_options', function($optionsHtml, $moduleSlug, $settings){
 	global $wp_roles;
 
