5ac1ee2a6ffbde7ae2b5009123016cb57a6d9128
Tsjippy <ewaldha@hotmail.com>
2024-10-11 07:15:08 +0100

    skip:

--- php/__module_menu.php
+++ php/__module_menu.php
@@ -3,6 +3,9 @@ namespace SIM\CONTENTFILTER;
 use SIM;
 
 const MODULE_VERSION		= '8.0.1';
+
+DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
+
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
 add_action('sim_module_activated', function($moduleSlug){
