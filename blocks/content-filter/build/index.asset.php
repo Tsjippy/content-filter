4f49c438a13c75ba53fa58626bf935936783ec64
Tsjippy <ewaldha@hotmail.com>
2026-09-24 07:28:42 +0200

    skip:

--- blocks/content-filter/build/index.asset.php
+++ blocks/content-filter/build/index.asset.php
@@ -1,1 +1,16 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-url'), 'version' => '46ebda1df62bf63a7110');
+<?php return array(
+	'dependencies' => array(
+		'react-jsx-runtime',
+		'wp-api-fetch',
+		'wp-block-editor',
+		'wp-components',
+		'wp-compose',
+		'wp-core-data',
+		'wp-data',
+		'wp-element',
+		'wp-html-entities',
+		'wp-i18n',
+		'wp-url'
+	),
+	'version' => '46ebda1df62bf63a7110'
+);
